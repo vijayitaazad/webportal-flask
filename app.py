3f09394175ec9bf1a585a854220a832719efdc9b
tunny <23007725+vijayitaazad@users.noreply.github.com>
2020-04-07 21:33:22 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask import render_template
+from templates import *
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+	return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
